275432ce4522f658f9321f3020a744d8a4d3b11b
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-07 14:53:29 -0400

    Updated from Brython Server: 6/7/2018 2:53:27 PM

--- newton'sMethod.py
+++ newton'sMethod.py
@@ -1,1 +1,20 @@
+#Liam Collins
+#newton's method
 
+def newtonMethod(x0):
+    f = 'x**2 + x**1 + 5'
+    h = 0.00001
+    fn = f.replace('x',str(x0))
+    fderiv = (f.replace('x',str(x0 + h))-f.replace('x',str(x0)))/h
+    
+    print(eval(fderiv))
+    
+    #xn = x0 - eval(f)/
+    
+    #print('f(x) = ',a,'x^',b,' + ',c,'x^',d,' + ',e,'     x0 =',x0)
+    
+    
+    
+    
+    
+newtonMethod(2)
