9f25a55bb1bdaeb97c93449974ec22a9f33e57b7
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-08 09:34:44 -0400

    Updated from Brython Server: 6/8/2018 9:34:44 AM

--- newton'sMethod.py
+++ newton'sMethod.py
@@ -4,20 +4,11 @@
 def newtonMethod(x0):
     f = '(x)**3 + (x)**1 + 5'
     h = 0.00001
-    for i in range(0,10):
+    for i in range(0,20):
         fn = eval(f.replace('x',str(x0)))
         defderiv = x0 + h
         fderiv = (eval(f.replace('x',str(defderiv)))-eval(f.replace('x',str(x0))))/h
         x0 = x0 - fn/fderiv
-        print(round(x0,7))
     print('zero at x =',round(x0,7))
     
-    #xn = x0 - eval(f)/
-    
-    #print('f(x) = ',a,'x^',b,' + ',c,'x^',d,' + ',e,'     x0 =',x0)
-    
-    
-    
-    
-    
-newtonMethod(-2)
+newtonMethod(5)
