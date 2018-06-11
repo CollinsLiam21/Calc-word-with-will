699e921368de269d8d729be1ceb8c902fb271d28
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-11 14:24:35 -0400

    Updated from Brython Server: 6/11/2018 2:24:34 PM

--- eulersmethod.py
+++ eulersmethod.py
@@ -1,1 +1,15 @@
+#Liam Collins
+#Euler's Method
 
+def euler(x,y,finalx,n):
+    fderiv = '2y + x'
+    dx = (finalx - x)/n
+    for i in range(0,n):
+        fderiv.replace('y',str(y))
+        fderiv.replace('x',str(x))
+        dy = eval(fderiv)*dx
+        x += dx
+        y += dy
+        print(x,y)
+    
+euler(1,2,3,4)
