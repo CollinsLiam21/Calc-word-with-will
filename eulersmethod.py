5c87715d98509661f55cffab6ac753e2d404ddf0
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-11 14:44:19 -0400

    Updated from Brython Server: 6/11/2018 2:44:18 PM

--- eulersmethod.py
+++ eulersmethod.py
@@ -2,13 +2,12 @@
 #Euler's Method
 
 def euler(x,y,finalx,n):
-    fderiv = '2*(y) + 1*(x)'
+    fderiv = '1*(y) + 1*(x)'
     dx = (finalx - x)/n
     for i in range(0,n):
         fderiv.replace('y',str(y))
         fderiv.replace('x',str(x))
         dy = eval(fderiv)*dx
-        print(dy)
         x += dx
         y += dy
         print('(',round(x,4),',',round(y,4),')')
