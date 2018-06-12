650dfed499045d5d6d88b5ca89009f35007c8726
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-12 14:19:52 -0400

    Updated from Brython Server: 6/12/2018 2:19:51 PM

--- limit.py
+++ limit.py
@@ -8,6 +8,13 @@ green = Color(0x00FF00,1)
 blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 
+X = 5
+Y = 5
+
+f = '2*(x)**2 + 2*(x)**1'
+for i in range(0,100):
+    Sprite(blackCircle, (i*X,500-eval(f.replace('x',str(i*X)))))
+
 blackOutline = LineStyle(1,black)
 
 #redRectangle = RectangleAsset(200,100,blackOutline,red) #width, height, outline, fill
@@ -16,7 +23,6 @@ blackLine = LineAsset(600,0,blackOutline) #x_endpoint, y_endpoint, lineStyle
 blackVertLine = LineAsset(0,500,blackOutline)
 text = TextAsset('Literature',fill=green, style='bold 40pt Times') #text, other options
 
-Sprite(blackCircle)
 Sprite(blackLine, (0,500))
 Sprite(blackVertLine)
 
