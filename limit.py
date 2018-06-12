01a437f734f00457088bdb8d7a2fddd0e2543daa
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-12 14:41:42 -0400

    Updated from Brython Server: 6/12/2018 2:41:40 PM

--- limit.py
+++ limit.py
@@ -3,6 +3,7 @@
 
 from ggame import *
 
+#Graphics
 red = Color(0xFF0000,1)
 green = Color(0x00FF00,1)
 blue = Color(0x0000FF,1)
@@ -10,11 +11,13 @@ black = Color(0x000000,1)
 
 blackOutline = LineStyle(1,black)
 
-#redRectangle = RectangleAsset(200,100,blackOutline,red) #width, height, outline, fill
-blackCircle = CircleAsset(5,blackOutline,black) #radius, outline, fill
-blackLine = LineAsset(600,0,blackOutline) #x_endpoint, y_endpoint, lineStyle
+blackCircle = CircleAsset(5,blackOutline,black) 
+blackLine = LineAsset(600,0,blackOutline) 
 blackVertLine = LineAsset(0,500,blackOutline)
-text = TextAsset('Literature',fill=green, style='bold 40pt Times') #text, other options
+text = TextAsset('Literature',fill=green, style='bold 40pt Times') 
+
+Sprite(blackLine, (0,250))
+Sprite(blackVertLine, (255,0))
 
 X = 10
 Y = 5
@@ -23,8 +26,7 @@ f = str(input('f(x) = ?'))
 for i in range(-100,100):
     Sprite(blackCircle, (250+i*X,250-Y*eval(f.replace('x',str(i)))))
 
-Sprite(blackLine, (0,250))
-Sprite(blackVertLine, (255,0))
+
 
 
 App().run()
