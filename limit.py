0bb1cb99d982852a332d70aad8cc0e52486bcb18
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-13 13:28:48 -0400

    Updated from Brython Server: 6/13/2018 1:28:46 PM

--- limit.py
+++ limit.py
@@ -13,19 +13,23 @@ black = Color(0x000000,1)
 blackOutline = LineStyle(1,black)
 
 blackCircle = CircleAsset(5,blackOutline,black) 
-blackLine = LineAsset(600,0,blackOutline) 
+blackLine = LineAsset(1000,0,blackOutline) 
 blackVertLine = LineAsset(0,500,blackOutline)
 text = TextAsset('Literature',fill=green, style='bold 40pt Times') 
+blackPlaceHolder = LineAsset(0,15,blackOutline)
 
 Sprite(blackLine, (0,255))
-Sprite(blackVertLine, (255,0))
+Sprite(blackVertLine, (355,0))
 
 X = 10
 Y = 10
 
+for i in range(-35,100):
+    Sprite(blackPlaceHolder, (i*X,255))
+
 f = str(input('f(x) = ? '))
 for i in range(-100,100):
-    Sprite(blackCircle, (250+i*X,250-Y*eval(f.replace('x',str(i)))))
+    Sprite(blackCircle, (350+i*X,250-Y*eval(f.replace('x',str(i)))))
 
 
 App().run()
